5e5dbc0ba9d8d126b790a6ea54216d6b7473af40
Qiang <liqiang372@gmail.com>
2015-06-24 18:19:54 -0700

    Init
    Initialize the project with fullPage.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+    	'verticalCentered': false,
+    	'css3': true,
+    	'sectionsColor': ['#3C4F6A', '#F2EACF', '#267481', '#33435A'],
+    	'navigation': true,
+    	'navigationPosition': 'right',
+    	'navigationTooltips': ['Intro', 'About Me', 'Skills', 'Projects']
+
+    });
+});
