75714ffe286c5a15ea72dcd320f223a19bb8e20e
Qiang Li <qiangli@Qiangs-MacBook-Pro.local>
2015-07-24 21:41:44 -0700

    improve

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
-$(document).ready(function() {
+
+// Calculate the time for page loading
+// fake loading if duration is short than 3s
+var timeStart = Date.now();
+
+$(window).load(function(){
+    var timeDuration = Date.now() - timeStart;
+    if (timeDuration < 3000) {
+        setTimeout(function(){
+        $(".pre-loading").fadeOut('slow', function(){
+        $('.main-content').fadeIn('slow');
+    });
+    }, 3000 - timeDuration);
+    } 
+    else {
+        $(".pre-loading").fadeOut('slow', function(){
+        $('.main-content').fadeIn('slow');
+    });
+    }
+
+
 
     // to ensure the moreaboutme panel only show once
-	var hasLoaded = false;
+    var hasLoaded = false;
     var firstTime = true;
     $('.info-container ul > li').each(function(){
        $(this).hide();
@@ -70,31 +90,31 @@ $(document).ready(function() {
 
 
     $('#fullpage').fullpage({
-    	'verticalCentered': false,
-    	'css3': true,
-    	'sectionsColor': ['#3C4F6A', '#EAAB28', '#578F7B', '#B74D3F','#F2EACF','#3d385a'],
-    	'navigation': true,
-    	'navigationPosition': 'right',
-    	'navigationTooltips': ['Intro', 'About Me', 'Skills', 'Projects', 'Contact Me'],
+        'verticalCentered': false,
+        'css3': true,
+        'sectionsColor': ['#3C4F6A', '#EAAB28', '#578F7B', '#B74D3F','#F2EACF','#3d385a'],
+        'navigation': true,
+        'navigationPosition': 'right',
+        'navigationTooltips': ['Intro', 'About Me', 'Skills', 'Projects', 'More About Me', 'Contact Me'],
 
-    	'afterLoad': function(anchorLink, index) {
+        'afterLoad': function(anchorLink, index) {
 
-    		if(index == 3) {
-    			// add the morphtext effect
-    			$(".text-change").Morphext({
-			        animation: "fadeInUp",
-			        complete: function () {
-			           // console.log("This is called after a phrase is animated in! Current phrase index: " + this.index);
-			        }
-			    });
-			    // show full list of skills
-			    $('h2').mouseover(function(){
+            if(index == 3) {
+                // add the morphtext effect
+                $(".text-change").Morphext({
+                    animation: "fadeInUp",
+                    complete: function () {
+                       // console.log("This is called after a phrase is animated in! Current phrase index: " + this.index);
+                    }
+                });
+                // show full list of skills
+                $('h2').mouseover(function(){
                     
-			    	var selected = $(this).next();
-			    	selected.css('opacity' , 0);
-			    	$(this).parent().find('.skill-list').addClass('visible');
-			    });
-			    $('h2').mouseout(function(){
+                    var selected = $(this).next();
+                    selected.css('opacity' , 0);
+                    $(this).parent().find('.skill-list').addClass('visible');
+                });
+                $('h2').mouseout(function(){
                    
                     var that = $(this);
                     var list = $(this).parent().find('.skill-list');
@@ -108,29 +128,32 @@ $(document).ready(function() {
 
                     });
 
-			    	
-					
-			    });
+                    
+                    
+                });
 
-    		}
+            }
 
-    		if(index == 5 && !hasLoaded) {
-    			hasLoaded = true;
-    			var delay = 1000;
-    			$('.info-container ul > li').each(function(){
-    				var that = $(this);
-    				setTimeout(function(){
-    					that.show();
-    					if(that.hasClass('dislike')){
-    						that.addClass('animated shake');
-    					} else {
-    						that.addClass('animated bounceIn');
-    					}
-    				}, delay);
-    				delay += 2000;
-    			})
-    		}
-    	}
+            if(index == 5 && !hasLoaded) {
+                hasLoaded = true;
+                var delay = 1000;
+                $('.info-container ul > li').each(function(){
+                    var that = $(this);
+                    setTimeout(function(){
+                        that.show();
+                        if(that.hasClass('dislike')){
+                            that.addClass('animated shake');
+                        } else {
+                            that.addClass('animated bounceIn');
+                        }
+                    }, delay);
+                    delay += 2000;
+                })
+            }
+        }
 
     });
+
+    
 });
+
